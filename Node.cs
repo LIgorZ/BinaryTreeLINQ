4277138cb73c09960e220eeff31d3281e19403bd
LIgorZ <96433539+LIgorZ@users.noreply.github.com>
2022-01-09 15:25:44 +0300

    Add project files.

---
+++ Node.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace BinaryTreeLINQ
+{
+    public class InfoTestStudent
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public string NameTest
+        {
+            get;
+            set;
+        }
+        public string DateTest
+        {
+            get;
+            set;
+        }
+    }
+    public class Node<T>
+        where T : IComparable<T>
+    {
+        public T Data
+        {
+            get;
+            set;
+        }
+        public InfoTestStudent infoTestStudent;
+        public Node<T> Left
+        {
+            get;
+            set;
+        }
+        public Node<T> Right
+        {
+            get;
+            set;
+        }
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+        public Node(T data, string name, string nameTest, string dateTest)
+        {
+            infoTestStudent = new InfoTestStudent();
+            Data = data;
+            infoTestStudent.Name = name;
+            infoTestStudent.NameTest = nameTest;
+            infoTestStudent.DateTest = dateTest;
+        }
+
+        public void Add(T data)
+        {
+            var node = new Node<T>(data);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data);
+                }
+            }
+        }
+        public void Add(T data, string name, string nameTest, string dateTest)
+        {
+            var node = new Node<T>(data, name, nameTest, dateTest);
+
+            if (node.Data.CompareTo(Data) == -1)
+            {
+                if (Left == null)
+                {
+                    Left = node;
+                }
+                else
+                {
+                    Left.Add(data, name, nameTest, dateTest);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = node;
+                }
+                else
+                {
+                    Right.Add(data, name, nameTest, dateTest);
+                }
+            }
+        }
+        public int CompareTo([AllowNull] T other)
+        {
+            return Data.CompareTo(other);
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace BinaryTreeLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NET.C#.12 Введение в LINQ");
+
+            var tree = new Tree<int>();
+            
+            string path = @"D:\Student.txt";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("5");
+                    sw.WriteLine("Иван Грозный");
+                    sw.WriteLine("Государство и право");
+                    sw.WriteLine("XVI век");
+
+                    sw.WriteLine("7");
+                    sw.WriteLine("Владимир Ленин");
+                    sw.WriteLine("Туризм");
+                    sw.WriteLine("XX век");
+
+                    sw.WriteLine("1");
+                    sw.WriteLine("Цезарь");
+                    sw.WriteLine("Математика");
+                    sw.WriteLine("I век до нэ");
+
+                    sw.WriteLine("2");
+                    sw.WriteLine("Роман");
+                    sw.WriteLine("Химия");
+                    sw.WriteLine("15/11/2021");
+
+                    sw.WriteLine("6");
+                    sw.WriteLine("Максим");
+                    sw.WriteLine("Физика");
+                    sw.WriteLine("11/10/2020");
+
+                    sw.WriteLine("8");
+                    sw.WriteLine("Федор");
+                    sw.WriteLine("История");
+                    sw.WriteLine("2/04/2018");
+
+                    sw.WriteLine("3");
+                    sw.WriteLine("Ли Сы");
+                    sw.WriteLine("Вирусология");
+                    sw.WriteLine("18/11/2019");
+
+                    sw.Close();
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                int mark;
+                string name, nameTask, dateTask;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    mark = int.Parse(line);
+                    name = sr.ReadLine();
+                    nameTask = sr.ReadLine();
+                    dateTask = sr.ReadLine();
+
+                    tree.Add(mark, name, nameTask, dateTask);
+                }
+
+                sr.Close();
+            }
+
+            var selectedStudent = tree.InOrderFull().Where(s => s.ToUpper().Contains("В")).OrderByDescending(s => s).Take(5);
+
+            Console.WriteLine("Выборка с сортировкой и с ограничением количества записей");
+            foreach (string s in selectedStudent)
+                Console.WriteLine(s);
+
+            Console.WriteLine("Вывод преордер");
+            foreach (var item in tree.PreOrder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Вывод постордер");
+            foreach (var item in tree.PostOrder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Вывод инордер");
+            foreach (var item in tree.InOrder())
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Оценка   ФИО    Название теста   Дата сдачи теста");
+
+            foreach (var item in tree.InOrderFull())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
